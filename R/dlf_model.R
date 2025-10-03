1fd466b7b17f060e1025287b2a8fb619ebf95171
Sharat Chandra <sharat.chndr@gmail.com>
2025-10-03 16:53:37 -0400

    Create dlf_model.R
    DPP4 inhibitor prediction R package

---
+++ R/dlf_model.R
@@ -0,0 +1,9 @@
+#' @name dlf_model
+#' @title Prediction of potential lead for DPPIV inhibition.
+#' @export
+#' @description A free webservice for all to predict the inhibitor of DPP4.
+#' @docType data
+#' @usage dlf_model
+#' @format a \code{\link{svm}} object
+#' @source Central Drug Research Institute - CDRI
+NULL
